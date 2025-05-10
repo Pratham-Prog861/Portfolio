b92e786c0e40ab212e15a558f129abca83f9bb35
Pratham Darji <155282402+Pratham-Prog861@users.noreply.github.com>
2025-05-10 19:36:31 +0530

    remove unnecessary newline characters

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -2,9 +2,9 @@
 import { TextGenerateEffect } from "./ui/text-generate-effect"
 
 const Text = () => {
-  const words = `Hi , I am Pratham Darji.\n
-  I am a Frontend Developer.\n
-  Expert in React, Next.js, Tailwind CSS, and TypeScript.\n
+  const words = `Hi , I am Pratham Darji.
+  I am a Frontend Developer.
+  Expert in React, Next.js, Tailwind CSS, and TypeScript.
   📍 Based in Vadodara, Gujarat, India.
   `;
   return (
